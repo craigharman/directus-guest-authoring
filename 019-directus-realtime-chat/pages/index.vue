a2355c87c49ea480c16b9c0b834666f4712810bb
Craig Harman <324026+craigharman@users.noreply.github.com>
2025-02-27 08:15:54 +0800

    Appending message is working

--- 019-directus-realtime-chat/pages/index.vue
+++ 019-directus-realtime-chat/pages/index.vue
@@ -1,22 +1,62 @@
 <script setup lang="ts">
+import type { SubscriptionOptionsEvents, WebSocketEvents } from '@directus/sdk'
+
 
 const { $directus } = useNuxtApp()
 
 const authToken: Ref<string | undefined> = ref()
+const messageList: Ref<Message[]> = ref([])
 
 onMounted(() => {
-	$directus.onWebSocket('open', () => {
-		console.log('Connection is open')
+	const cleanup = $directus.onWebSocket('message', (data) => {
+		if (data.type === 'auth' && data.status === 'ok') {
+			authToken.value = data.refresh_token
+			readAllMessages()
+			subscribe('create')
+		}
+
+		if (data.type === 'items') {
+			for (const item of data.data) {
+				addMessageToList(item)
+			}
+		}
+		console.log(data)
 	})
 
-	$directus.onWebSocket('message', (event) => {
-		if (event.type === 'auth' && event.status === 'ok') {
-			authToken.value = event.refresh_token
-			subscribe()
-		}
-		console.log('Message received:', event)
-	})
+	$directus.connect()
+	onBeforeUnmount(cleanup)
 })
+
+const addMessageToList = (message) => {
+	messageList.value.push(message)
+}
+
+const readAllMessages = () => {
+	$directus.sendMessage({
+		type: 'items',
+		collection: 'messages',
+		action: 'read',
+		query: {
+			limit: 10,
+			sort: '-date_created',
+			fields: ['*', 'user_created.first_name'],
+		},
+	})
+}
+
+// onMounted(() => {
+// 	$directus.onWebSocket('open', () => {
+// 		console.log('Connection is open')
+// 	})
+
+// 	$directus.onWebSocket('message', (event) => {
+// 		if (event.type === 'auth' && event.status === 'ok') {
+// 			authToken.value = event.refresh_token
+// 			subscribe() // Subscribe to all events
+// 		}
+// 		console.log('Message received:', event)
+// 	})
+// })
 
 const credentials = ref({
 	email: '',
@@ -37,16 +77,27 @@ const login = async () => {
 	$directus.sendMessage(JSON.stringify(login))
 }
 
-async function subscribe() {
+const subscribe = async (event) => {
 	const { subscription } = await $directus.subscribe('messages', {
-		event: 'create',
+		event,
 		query: {
 			fields: ['*', 'user_created.first_name'],
 		},
 	})
 
-	for await (const item of subscription) {
-		console.log(item)
+	for await (const message of subscription) {
+		console.log('receiveMessage', message)
+		receiveMessage(message)
+	}
+}
+
+const receiveMessage = (data) => {
+	if (data.type === 'subscription' && data.event === 'init') {
+		console.log('subscription started')
+	}
+	if (data.type === 'subscription' && data.event === 'create') {
+		const message = data.data[0]
+		addMessageToList(message)
 	}
 }
 </script>
@@ -62,6 +113,9 @@ async function subscribe() {
 		<div v-else>
 			<h2>Chat</h2>
 			<div>Logged in</div>
+			<div v-for="message in messageList" :key="message.id">
+				{{ message.user_created.first_name }}: {{ message.content }}
+			</div>
 		</div>
 	</div>
 </template>
