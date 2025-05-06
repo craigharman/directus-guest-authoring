5a1b3a9ea81e50045169fea5e6f4ed77f50c7d99
Craig Harman <324026+craigharman@users.noreply.github.com>
2025-05-06 18:29:17 +0800

    Added authentication to 048

--- 048-directus-submit-forms/plugins/directus.ts
+++ 048-directus-submit-forms/plugins/directus.ts
@@ -4,10 +4,12 @@ import {
 	readItem,
 	readItems,
 	withToken,
+	authentication,
 } from "@directus/sdk";
 const directus = createDirectus<DirectusSchema>(
-	"http://your-website-url/directus",
-).with(rest());
+	"http://localhost:3000/directus",
+).with(authentication("session", { credentials: "include" }))
+.with(rest({ credentials: "include" }));
 export default defineNuxtPlugin(() => {
 	return {
 		provide: { directus, readItem, readItems, withToken },
