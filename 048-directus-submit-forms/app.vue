9242e4af5752d4cf2efe2496388a2ddf0fc5f514
Craig Harman <324026+craigharman@users.noreply.github.com>
2025-05-08 17:45:32 +0800

    Dynamic form building

--- 048-directus-submit-forms/app.vue
+++ 048-directus-submit-forms/app.vue
@@ -1,6 +1,41 @@
+<script setup lang="ts">
+import { createItem, readItem, type QueryItem } from '@directus/sdk';
+
+const { $directus } = useNuxtApp()
+
+const formId = "5da3d356-d818-434f-b225-db35c418bbb6"
+const form: Ref<Form | undefined> = ref()
+const name = ref('');
+const email = ref('')
+
+
+$directus.request(readItem('forms', formId, {
+  fields: ['*', 'fields.*']}))
+  .then((response) => {
+    console.log('Form data:', response);
+    form.value = response;
+  })
+  .catch((error) => {
+    console.error('Error fetching form data:', error);
+  });
+
+const fields: Ref<FormSubmissionItem[]> = ref([])
+
+const submit = () => {
+  $directus.request(createItem('form_submissions', {
+    form: formId,
+    values: fields.value
+  }))
+}
+</script>
+
 <template>
-  <UApp>
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
-  </UApp>
+    <form v-if="form" @submit.prevent="submit">
+      <!-- TODO: How to use a single object to save v-models???? -->
+      <div v-for="(field, index) in form.fields" :key="index">
+        <label :for="field.name">{{ field.label }}</label>
+        <input :name="field.name" v-model="field.value" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
 </template>
--- 048-directus-submit-forms/directus.d.ts
+++ 048-directus-submit-forms/directus.d.ts
@@ -1,9 +1,40 @@
 /// <reference types="@directus/extensions/api.d.ts" />
 interface DirectusSchema {
-	posts: Post[];
+	forms: Form;
+	form_submissions: FormSubmission;
 }
-interface Post {
-	id: number;
+interface Form {
+	id: string;
+	sort: number | null;
 	title: string;
-	content: string;
+	is_active: boolean;
+	submit_label: string;
+	on_success: string;
+	success_message: string;
+	success_redirect_url: string | null;
+	fields: FormField[];
+}
+
+interface FormField {
+	id: string;
+	name: string;
+	type: string;
+	label: string;
+	placeholder: string | null;
+	help: string | null;
+	validation: string | null;
+	width: string;
+	choices: any | null;
+	form: string;
+	sort: number;
+	required: boolean;
+}
+interface FormSubmission {
+	form: string;
+	values: FormSubmissionItem[]
+}
+
+interface FormSubmissionItem {
+	field: string;
+	value: string;
 }
